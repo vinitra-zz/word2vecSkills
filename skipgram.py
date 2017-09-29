596a220cbdbad94909a47afbed6bc2655d08bddf
wiltonwu <wiltonwu@berkeley.edu>
2017-09-29 13:46:13 -0700

    skipgram model

---
+++ skipgram.py
@@ -0,0 +1,5 @@
+from gensim.models import Word2Vec
+from gensim.models.word2vec import LineSentence
+
+sentences = LineSentence('skill.tsv')
+model = Word2Vec(sentences, size=100, window=5, min_count=10, workers=10, iter=30)
