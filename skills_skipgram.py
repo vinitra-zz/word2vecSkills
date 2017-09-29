cc063b2b1fb2473bcd9006903e528af68f33fe4a
wiltonwu <wiltonwu@berkeley.edu>
2017-09-29 13:49:49 -0700

    rename to skills

--- skills_skipgram.py
+++ skills_skipgram.py
@@ -1,5 +1,6 @@
 from gensim.models import Word2Vec
 from gensim.models.word2vec import LineSentence
 
+# Number 1
 sentences = LineSentence('skill.tsv')
 model = Word2Vec(sentences, size=100, window=5, min_count=10, workers=10, iter=30)
